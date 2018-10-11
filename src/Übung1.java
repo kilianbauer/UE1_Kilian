f80e79ea5df7081c5e75d00fd6ed12b722c9b439
kilianbauer <1710653657@stud.fh-kufstein.ac.at>
2018-10-11 15:48:23 +0200

    final commit

--- src/Übung1.java
+++ src/Übung1.java
@@ -1,12 +1,12 @@
-import java.sql.Date;
-
 public class Übung1 {
 
     public static void main(String[]args){
         String name = "Kilian ";
         String surname = "Bauer";
+        int birthdate = 1995;
         int age = 23;
-        System.out.println("Hello " + name + surname + "! Du bist " + age + " Jahre alt");
+        String wohnort = "Rosenheim";
+        System.out.println("Hello " + name + surname + "! Du bist " + age + " Jahre alt, geboren " + birthdate +" und lebst in " + wohnort);
 
     }
 
