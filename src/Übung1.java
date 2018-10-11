469face26a92e07e906007ea992646c03293e2ad
kilianbauer <1710653657@stud.fh-kufstein.ac.at>
2018-10-11 15:40:10 +0200

    commit 3

--- src/Übung1.java
+++ src/Übung1.java
@@ -1,10 +1,13 @@
+import java.sql.Date;
+
 public class Übung1 {
 
     public static void main(String[]args){
-        String name = "Kilian";
+        String name = "Kilian ";
+        String surname = "Bauer";
         int age = 23;
-        System.out.println("Hello" + name + "!");
-        System.out.println(age);
+        System.out.println("Hello " + name + surname + "! Du bist " + age + " Jahre alt");
+
     }
 
 }
