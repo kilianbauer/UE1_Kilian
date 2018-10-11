31647b98abac54d2967a3a044434185c56d5b708
kilianbauer <1710653657@stud.fh-kufstein.ac.at>
2018-10-11 15:32:57 +0200

    dritter commit

--- src/Übung1.java
+++ src/Übung1.java
@@ -2,8 +2,9 @@ public class Übung1 {
 
     public static void main(String[]args){
         String name = "Kilian";
-
+        int age = 23;
         System.out.println("Hello" + name + "!");
+        System.out.println(age);
     }
 
 }
